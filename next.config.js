637a8c3508e34777fff5710f591269153be9ee96
Carlos Precioso <kalprestito@gmail.com>
2020-02-29 14:49:56 +0100

    Disable minification for mjml

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
-  webpack(config, { isServer }) {
+  webpack(config, { isServer, dev }) {
+    if (!dev) {
+      const prevOptions = config.optimization.minimizer[0].options
+      config.optimization.minimizer[0] = new TerserPlugin({
+        ...prevOptions,
+        test: new RegExp(
+          `^${path.basename(require.resolve("mjml/package.json"))}.+$`
+        )
+      })
+    }
+
     if (!isServer) {
       ;(config.resolve || (config.resolve = {})).alias ||
         (config.resolve.alias = {})
